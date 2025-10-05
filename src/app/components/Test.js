3ca82a207bad645b8a2dd52d288230357dc15b1c
Sean Kozen <sgkozen@gmail.com>
2025-10-05 22:20:08 +0900

    gitignore edit

--- src/app/components/Test.js
+++ src/app/components/Test.js
@@ -10,18 +10,18 @@
 //   return <pre>{JSON.stringify(projects, null, 2)}</pre>;
 // }
 
-import { createClient } from "next-sanity";
+// import { createClient } from "next-sanity";
 
-const client = createClient({
-  projectId: "8cp5cptd", // hard-coded for testing
-  dataset: "portfolio-blog",
-  apiVersion: "2023-10-01",
-  useCdn: false, // ensure fresh data
-});
+// const client = createClient({
+//   projectId:  // hard-coded for testing
+//   dataset:
+//   apiVersion:
+//   useCdn: false, // ensure fresh data
+// });
 
-export default async function TestPage() {
-  const projects = await client.fetch(`*[_type=="project"]{_id, name}`);
-  console.log("Fetched projects:", projects);
+// export default async function TestPage() {
+//   const projects = await client.fetch(`*[_type=="project"]{_id, name}`);
+//   console.log("Fetched projects:", projects);
 
-  return <pre>{JSON.stringify(projects, null, 2)}</pre>;
-}
+//   return <pre>{JSON.stringify(projects, null, 2)}</pre>;
+// }
