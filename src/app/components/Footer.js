9a78322f48e3d8391351ddb9790dbcce33acc7ef
Sean Kozen <sgkozen@gmail.com>
2026-04-04 18:25:48 +0900

    Footer copyright date

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 function Footer() {
+  const year = new Date().getFullYear();
   return (
     <footer className="w-full border-t border-gray-500 py-8">
       <div className="container mx-auto text-center">
         <p className="text-gray-500">
-          &copy; 2025 Sean Kozen — All Rights Reserved
+          &copy; {year} Sean Kozen — All Rights Reserved
         </p>
       </div>
     </footer>
